c4cfb2b91127eb90b5a1a7115dd0742c36a5ed32
John <jw4429@nyu.edu>
2020-04-29 15:49:00 -0400

    I can change I can change I can change. If it helps you fall in loooooooooveeeee

--- Game V2/Assets/Scripts/Worker Classes/Intro.cs
+++ Game V2/Assets/Scripts/Worker Classes/Intro.cs
@@ -7,32 +7,98 @@ using UnityEngine.UI;
 public class Intro : MonoBehaviour
 {
     public float timerOne = 150f;
-    public float timerTwo = 150f;
+    public float timerTwo = 200f;
+    public float timerThree = 400f;
+    public float timerFour = 200f;
+    public float timerFive = 150f;
 
-    public bool line1Done;
-    public bool line2Done;
-
-
-    public String line1 = "";
-
-    public Text box;
+    public bool line1Done = false;
+    public bool line2Done = false;
+    public bool line3Done = false;
+    public bool line4Done = false;
+    public bool line5Done = false;
+    
+    public String line1 = "Your father and I think it’s time for you to know your roots and visit your Uncle Lucca.";
+    public String line2 = "I know you don’t know each other but I called him and he seems excited to see his niece, maybe he can bake you some of his famous focaccia!";
+    public String line3 = "It’s a lovely little town, it will be nice to get away from the city for awhile. Your grandfather loved that place and you can visit the bakery and understand why it means so much to him. It’s been a long time since they spoke, hopefully you can be the reason for them to talk again.";
+    public String line4 = "Your things are already at the hotel, it’s just a week so try to enjoy it while you can. Get acquainted with the town, everyone is very welcoming.";
+    public String line5 = "I love you honey, have a good time. Try to get that focaccia recipe!";
+    
+    public GameObject box;
+    public GameObject background;
+    public GameObject canvas;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        canvas.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+        box.gameObject.SetActive(true);
+        box.GetComponent<TypingEffect>().text = line1;
+        StartCoroutine(box.GetComponent<TypingEffect>().AnimateText());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //stop coroutine
-        //display text
-        //fade in
-        //clear text
-        //fade out
-        //display text
-        //fade in 
-        //repeat
+        if (line1Done == false && line2Done == false)
+        {
+            timerOne -= Time.deltaTime;
+            if (timerOne < 0)
+            {
+                line1Done = true;
+                StopCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+                box.GetComponent<TypingEffect>().text = line2;
+            }
+        }
+        
+        if (line1Done == true && line2Done == false)
+        {
+            StartCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+            timerTwo -= Time.deltaTime;
+            if (timerTwo < 0)
+            {
+                line2Done = true;
+                StopCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+                box.GetComponent<TypingEffect>().text = line3;
+            }
+        }
+        
+        if (line2Done == true && line3Done == false)
+        {
+            StartCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+            timerThree -= Time.deltaTime;
+            if (timerThree < 0)
+            {
+                line3Done = true;
+                StopCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+                box.GetComponent<TypingEffect>().text = line4;
+            }
+        }
+        
+        if (line3Done == true && line4Done == false)
+        {
+            StartCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+            timerFour -= Time.deltaTime;
+            if (timerFour < 0)
+            {
+                line4Done = true;
+                StopCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+                box.GetComponent<TypingEffect>().text = line5;
+            }
+        }
+        
+        if (line4Done == true && line5Done == false)
+        {
+            StartCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+            timerFive -= Time.deltaTime;
+            if (timerFive < 0)
+            {
+                line5Done = true;
+                StopCoroutine(box.GetComponent<TypingEffect>().AnimateText());
+                box.GetComponent<TypingEffect>().text = " ";
+            }
+        }
+        
     }
 }
--- Game V2/Assets/Scripts/Worker Classes/TypingEffect.cs
+++ Game V2/Assets/Scripts/Worker Classes/TypingEffect.cs
@@ -7,29 +7,27 @@ public class TypingEffect : MonoBehaviour
 {
     public Text textBox;
     //Store all your text in this string array
-    string[] goatText = new string[]{"1. Laik's super awesome custom typewriter script", "2. You can click to skip to the next text", "3.All text is stored in a single string array", "4. Ok, now we can continue","5. End Kappa"};
-    int currentlyDisplayingText = 0;
-    void Awake () {
-        StartCoroutine(AnimateText());
-    }
-    //This is a function for a button you press to skip to the next text
-    public void SkipToNextText(){
-        StopAllCoroutines();
-        currentlyDisplayingText++;
-        //If we've reached the end of the array, do anything you want. I just restart the example text
-        if (currentlyDisplayingText>goatText.Length) {
-            currentlyDisplayingText=0;
-        }
-        StartCoroutine(AnimateText());
-    }
+    public string text = " ";
+
+    public bool done = false;
+    //int currentlyDisplayingText = 0;
+    
+    
     //Note that the speed you want the typewriter effect to be going at is the yield waitforseconds (in my case it's 1 letter for every      0.03 seconds, replace this with a public float if you want to experiment with speed in from the editor)
-    IEnumerator AnimateText(){
+    
+    //StartCoroutine(AnimateText());
+    public IEnumerator AnimateText(){
      
-        for (int i = 0; i < (goatText[currentlyDisplayingText].Length+1); i++)
+        for (int i = 0; i < (text.Length+1); i++)
         {
-            textBox.text = goatText[currentlyDisplayingText].Substring(0, i);
+            textBox.text = text.Substring(0, i);
+            if (i == (text.Length + 1))
+            {
+                done = true;
+            }
             yield return new WaitForSeconds(.03f);
         }
+        
     }
 }
 
